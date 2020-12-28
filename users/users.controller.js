cc4e5bb266c8a29fb0684bda2afb4e8d02b08dc5
Muralimanokar <52714786+Muralimanokar@users.noreply.github.com>
2020-12-28 17:59:17 +0530

    Add files via upload

---
+++ users/users.controller.js
@@ -0,0 +1,37 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+const authorize = require('_helpers/authorize')
+const Role = require('_helpers/role');
+
+
+router.post('/authenticate', authenticate);     
+router.get('/', authorize(Role.Admin), getAll); 
+router.get('/:id', authorize(), getById);       
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users => res.json(users))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    const currentUser = req.user;
+    const id = parseInt(req.params.id);
+
+   
+    if (id !== currentUser.sub && currentUser.role !== Role.Admin) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
